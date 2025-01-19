f9e77502df8d9ee5a943631bb9fc6f9c414ebd99
pwatjana <ppwj10976@gmail.com>
2025-01-19 16:17:10 +0700

    Create SQL_1.py

---
+++ SQL_1.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+#Create data frame name 'employees'
+employees = pd.DataFrame({
+    'employee_id' : [None, None, 1188, 1189, 7790],
+    'office': [None,None, 'West-123', 'West-987', 'East-417'],
+    'device_id': ['a89fgbd63f0fg', 'y5g2h5f5dghu', 'yj5df7e2g5h8d', 'e2g5o9w6s35q', 'q3j7d1s2v5b12']
+})
+
+#Create data frames name 'machines'
+machines = pd.DataFrame({
+    
+})
